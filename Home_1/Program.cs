8b4893b97abc687e6a343c7d31d79c87227dde8f
user <user@WIN-KCAN6I1U7SJ>
2023-03-30 19:36:22 +0300

    Добавьте файлы проекта.

---
+++ Home_1/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя и нажмите ENTER");
+var name = Console.ReadLine();
+Console.WriteLine("Hello, " + name + "!");
